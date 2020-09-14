818b68977299e8a4f33dfc35d326d4f007729120
Matt D'Correa <matiasdylanc@gmail.com>
2020-09-14 16:08:52 -0300

    agregar productos agregado

--- src/routes/products.js
+++ src/routes/products.js
@@ -41,17 +41,39 @@ router.post('/', (req, res) => {
 
 // promesas
 // crear producto
+// router.post('/products', (req, res)=>{
+//   Product.create({
+//    title: 'Manualmente insercion 3',
+//    description: "Manualmente insercion descripcion 3"  
+//   }) 
+//    .then(doc=>{
+//        res.json(doc)
+//    }).catch(err=>{
+//        console.log(err); 
+//        res.json(err)  
+//    })
+// })
+
 router.post('/products', (req, res)=>{
-  Product.create({
-   title: res.body.title,
-   description: req.body.description  
-  }) 
-   .then(doc=>{
-       res.json(doc)
-   }).catch(err=>{
-       console.log(err); 
-       res.json(err)  
-   })
+  let body = req.body;
+  console.log(body);
+  const product = new Product({
+    title: body.title,
+    description: body.description
+  });
+  product.save((err, productSaved) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        mensaje: 'error al crear el producto',
+        errors: err
+      })
+    }
+    res.json({
+      ok: true,
+      mensaje: productSaved
+    })
+  })
 })
 
 // buscar productos
@@ -67,13 +89,7 @@ router.get('/products', (req, res) => {
 
 
 
-let attributes = ['title', 'description']
-let productParams = {}
 
-attributes.forEach(attr=>{
-  if(Object.prototype.hasOwnProperty.call(req.body,attr))
-  productParams = req.body(attr);
-})
 
 
 // buscar producto por
@@ -87,25 +103,35 @@ router.get('/products/:id', (req, res)=> {
     })
 })
 
-router.put('/products/:id', (req, res)=> {
-  // Product.findById(req.params.id)
-  // .then(doc => {
-  //   doc.title = req.params.title;
-  //   doc.description = req.params.description;
+// router.put('/products/:id', (req, res)=> {
+//   let attributes = ['title', 'description'];
+// let productParams = {};
+// attributes.forEach(attr=>{
+//   if(Object.prototype.hasOwnProperty.call(req.body,attr))
+//   productParams = req.body(attr);
+// })
+//   // Product.findById(req.params.id)
+//   // .then(doc => {
+//   //   doc.title = req.params.title;
+//   //   doc.description = req.params.description;
 
-  //   doc.save();
-  // })
-  Product.findByIdAndUpdate({'_id': req.params.id}, productParams
-  // {
-  //   title: req.body['title'],
-  //   description: req.body.description
-  // }
-  ).then(doc => {
-    res.json(doc);
-  }).catch(err => {
-    console.log(err);
-    res.json(err);
-  })
-})
+//   //   doc.save();
+//   // })
+//   Product.findByIdAndUpdate(req.params.id, productParams, {new: true}
+//   // {
+//   //   title: req.body['title'],
+//   //   description: req.body.description
+//   // }
+//   ).then(doc => {
+//     res.json(doc);
+//   }).catch(err => {
+//     console.log(err);
+//     res.json(err);
+//   })
+// })
+
+
+
+
 
 module.exports = router;
