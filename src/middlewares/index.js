66a438d22974804de637f581efb2c956d8eb387d
Matt D'Correa <matiasdylanc@gmail.com>
2020-09-21 17:58:10 -0300

    algo rompi

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,5 @@
 const {authJwt} = require('./authJwt');
-const {verefySignup} = require(',/verefySignup');
+const {verefySignup} = require('./verefySignup');
 
 module.exports.authJwt = authJwt;
 module.exports.verefySignup = verefySignup;
